59fead81a84c59c36e4dd87655c382f6aabaaef7
Mohit Varma <mohitvarma1993@gmail.com>
2020-12-22 01:48:56 -0500

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 //Unit Test
-test('Port should be 3001 and not 3000', () => {
+test('Port is 3001', () => {
     expect(server()).not.toBe(3000)
     expect(server()).toBe(3001)
 })
